b63f1df5bf354f061221ff77270986f4f73f4230
YunSongHui <dbsthdgml123@naver.com>
2020-01-14 18:48:01 +0900

    자료형 - 문자열 변환(replaceAll, toUpper/LowerCase)

--- java-study/src/test_package/DataType.java
+++ java-study/src/test_package/DataType.java
@@ -4,20 +4,12 @@ public class DataType {
 	
 	public static void main(String[] args) {
 		
-		String a = "hello"; 
-		String b = "java";
-		String c = "hello";
+		String f = "Hello Java";
+		System.out.println(f.replaceAll("Java","World")); //a.replaceAll("text1","text2") 문자열 a 중 특정 문자 text1 을 text2로 변환한다 
+		System.out.println(f.replaceAll("a","e")); //동일한 문자가 있을 경우 모두 바뀐다
 		
-		System.out.println(a.equals(b));  //문자열 비교 시 무조건 equals를 사용해야 한다
-		System.out.println(a.equals(c));
-		
-		
-		
-		String d = "hello"; //d와 e는 값은 같지만 서로 다른 객체
-		String e = new String("hello"); //new는 객체를 만들 때 사용한다. 근데 그냥 String a = "text" 라고 쓰는게 편하다.
-		
-		System.out.println(d.equals(e)); //true
-		System.out.println(d==e); //==는 두 개의 자료형이 동일한 객체인지를 판별하는 연산자이므로 false 리턴
+		System.out.println(f.toUpperCase()); //문자열을 모두 대문자로 바꾼다
+		System.out.println(f.toLowerCase()); //문자열을 모두 소문자로 바꾼다
 		
 	}
 
