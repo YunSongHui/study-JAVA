b933da52f9d0e0b47033779ce9393ecdad105c34
YunSongHui <dbsthdgml123@naver.com>
2020-01-29 14:57:33 +0900

    자료형 - 배열

--- java-study/src/test_package/DataType.java
+++ java-study/src/test_package/DataType.java
@@ -4,16 +4,18 @@ public class DataType {
 	
 	public static void main(String[] args) {
 		
-		String str = new String("hello java");
-		StringBuffer sb = new StringBuffer("hello java");
-
-		sb.replace(0,1,"H"); //StringBuffer sb 객체의 값 자체가 바뀐다
-		String str2 = str.replace('h','H'); //String str2 라는 새로운 객체 생성
-
-		System.out.println(sb);
-		System.out.println(str);
-		System.out.println(str2);
-
+		//배열이란 자료형의 종류가 아닌 자료형의 집합을 의미
+		int[] odds = {1,3,5,7,9}; //배열은 자료형[]으로 표현
+		String[] weeks = {"월", "화", "수", "목", "금", "토", "일"};
+		
+		//배열 값 접근
+		System.out.println(odds[2]); //배열의 0번째 값부터 세어 3번째 값 출력
+		
+		//배열의 길이는 .length를 사용
+		for (int i=0; i<weeks.length; i++) {
+			System.out.println(weeks[i]);
+		}
+		
 	}
-
+	
 }
