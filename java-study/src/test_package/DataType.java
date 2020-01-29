45e1700fa3544fbd0a79141b508f3623a313476e
YunSongHui <dbsthdgml123@naver.com>
2020-01-29 10:59:01 +0900

    자료형 - StringBuffer

--- java-study/src/test_package/DataType.java
+++ java-study/src/test_package/DataType.java
@@ -4,12 +4,18 @@ public class DataType {
 	
 	public static void main(String[] args) {
 		
-		String f = "Hello Java";
-		System.out.println(f.replaceAll("Java","World")); //a.replaceAll("text1","text2") 문자열 a 중 특정 문자 text1 을 text2로 변환한다 
-		System.out.println(f.replaceAll("a","e")); //동일한 문자가 있을 경우 모두 바뀐다
+		StringBuffer mybuffer = new StringBuffer(); //객체 생성
 		
-		System.out.println(f.toUpperCase()); //문자열을 모두 대문자로 바꾼다
-		System.out.println(f.toLowerCase()); //문자열을 모두 소문자로 바꾼다
+		mybuffer.append("hello"); //append : 문자열추가
+		mybuffer.append(" ");
+		mybuffer.append("jump to java");
+		mybuffer.append(" !");
+		
+		mybuffer.insert(0, "Oh "); //insert : 0번째 위치에 삽입
+		
+		System.out.println(mybuffer.toString()); //toString() : String자료형으로 변경
+		
+		System.out.println(mybuffer.substring(3,7)); //substring : 3번째 위치부터 7번째 위치까지의 문자열 출력
 		
 	}
 
