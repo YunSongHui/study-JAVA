93f6830a202cbc21b745e57b376b08169d87e247
YunSongHui <dbsthdgml123@naver.com>
2020-01-29 14:35:32 +0900

    자료형 - StringBuffer와 String의 replace 차이

--- java-study/src/test_package/DataType.java
+++ java-study/src/test_package/DataType.java
@@ -4,26 +4,16 @@ public class DataType {
 	
 	public static void main(String[] args) {
 		
-		//StringBuffer 사용
-		StringBuffer sb = new StringBuffer(); //객체 생성
-		
-		sb.append("hello"); 		//문자열 추가
-		sb.append(" "); 			//문자열 추가
-		sb.append("jump to java");  //문자열 추가
-		sb.append(" !"); 			//문자열 추가
-		
-		System.out.println(sb.toString()); //toString() : String 자료형으로 변경
-		
-		
-		//String 사용
-		String temp = ""; //객체 생성
-		
-		temp += "hello"; 		//객체 생성(String 자료형에 + 연산이 있을 때마다 새로운 String 객체 생성)
-		temp += " "; 			//객체 생성
-		temp += "jump to java"; //객체 생성
-		temp += " !"; 			//객체 생성
-		
-		System.out.println(temp);
+		String str = new String("hello java");
+		StringBuffer sb = new StringBuffer("hello java");
+
+		sb.replace(0,1,"H"); //StringBuffer sb 객체의 값 자체가 바뀐다
+		String str2 = str.replace('h','H'); //String str2 라는 새로운 객체 생성
+
+		System.out.println(sb);
+		System.out.println(str);
+		System.out.println(str2);
+
 	}
 
 }
